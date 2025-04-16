be99c9687d60a2c62f5b50a1d145f37bbdd43869
Earl Salimbot <juhniarl@gmail.com>
2025-04-16 08:32:01 +0800

    second commit upload to github

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from config import Config
 from app.utils.database import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
+    migrate = Migrate(app, db)  # Add this line
     
-    with app.app_context():
-        db.create_all()
+    # Remove the db.create_all() call - migrations will handle this
     
     from app import routes
     app.register_blueprint(routes.bp)
